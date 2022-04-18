50affdba41d62a0b9cc7159c3924cf9586c2f022
boggsey <eric@mlemedia.com>
2022-04-17 21:36:42 -0400

    Changing uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://my-app-9g6ss.ondigitalocean.app/',
   cache: new InMemoryCache(),
 });
 
